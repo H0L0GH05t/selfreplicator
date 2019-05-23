181f52dbf9269a6d843e49b0ad9bfc6c56c4d7c9
Arin Kase <arin.kase@gmail.com>
2019-05-23 14:53:30 -0700

    testing

--- selfreplicator/views.py
+++ selfreplicator/views.py
@@ -72,55 +72,8 @@ def create_repo(auth_token, username, result_msgs):
                                                                                  'auto_init': False})
     
     if response.status_code == 201:
-        created_repo_link = response.location
-
-        # List of files to replicate
-        appfiles = [
-            'Procfile',                                 #gunicorn procfile
-            'staticfiles',                              # empty dir to collect static with whitenoise
-            'Procfile.windows',                         # gunicorn for local windows
-            'README.md',                                # github: documentation
-            'requirements.txt',                         # list of all required libraries for python
-            'runtime.txt',                              # version of python to use at runtime
-            # 'db.sqlite3',                             # database file
-            'selfreplicator',                           # django app root folder
-            'selfreplicator/admin.py',                  # django: django admin page
-            'selfreplicator/__init__.py',               # django: generated init
-            'selfreplicator/apps.py',                   # django: generated app config
-            'selfreplicator/models.py',                 # django: model objects
-            'selfreplicator/views.py',                  # django: code each page view in urls
-            'selfreplicator/static',                    # static file location
-            'selfreplicator/static/app-logo.png',       # custom logo
-            'selfreplicator/static/selfreplicator.js',  # script for entire site
-            'selfreplicator/static/style.css',          # styles for entire site
-            'selfreplicator/templates',                 # folder for html django templates
-            'selfreplicator/templates/base.html',       # contains the base html for the site
-            'selfreplicator/templates/index.html',      # home page
-            'selfreplicator/templates/results.html',    # will show results message
-            'githubapps',                               # django project root folder
-            'githubapps/static',                        # empty static dir
-            'githubapps/static/humans.txt',             # blank file so dir is not empty
-            'githubapps/__init__.py',                   # django: generated init file
-            'githubapps/settings.py',                   # django: settings for project
-            'githubapps/urls.py',                       # django: url paths to use
-            'githubapps/wsgi.py']                       # django: wsgi settings for app
-        
-        for appfile in appfiles:
-            try:
-                # filepath relative from working dir (should be where manage.py is)
-                # open file we need to copy to repo and call replicate file
-                with open(appfile) as f:
-                    file_to_copy = f.read()
-                    replicate_file(file_to_copy, username)
-            except:
-                result_status = "error creating file"
-                result_msgs.append("Failed to create file: %s" % appfile)
-                pass
-            
-        if result_status == "success":
-            result_msgs.append("Successfully added files to repo!")
-        else:
-            result_msgs.append("Failed to add files to repo")
+        result_status = "success"
+        result_msgs.append("successfully created new repo")
     else:
         # record repo creation error message
         result_status = "error creating repo"
