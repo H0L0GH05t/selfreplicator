d2963110b78dcfff4011c46256c432fd6e5eba09
Arin Kase <arin.kase@gmail.com>
2019-05-22 20:10:53 -0700

    fixing api call

--- selfreplicator/views.py
+++ selfreplicator/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.conf import settings
 import requests
+import os
 
 def index(request):
     return render(request, "index.html")
@@ -11,24 +12,38 @@ def results(request):
     code_for_token = request.GET.get('code')
 
     auth_response = requests.post('https://github.com/login/oauth/access_token', params={'client_id':settings.CLIENT_ID, 'client_secret': settings.CLIENT_SECRET, 'code': code_for_token}) #Accept: application/json
+    if (auth_response.status_code != 200) or (auth_response.status_code !=201):
+        return(request, "error.html", {'error_msg': "There was a problem with authentication"})
     access_token = auth_response.text.split('&')[0].replace('access_token=', '')
-    auth_status = auth_response.status_code
     
     username_response = requests.get('https://api.github.com/user', params={'access_token': access_token})
+    if username_response.status_code != 200:
+        return(request, "error.html", {'error_msg': "There was a problem with finding your profile"})
     username = username_response.json().get('login')
-    test = username_response.text
-    user_status = auth_response.status_code
     
-    return render(request, "results.html", {'access_token': access_token,
-                                            'client_id': settings.CLIENT_ID,
-                                            'username': username,
-                                            'username_response' : user_status,
-                                            'test': test})
+    create_repo(auth_token, username)
+    
+    return render(request, "results.html", {'client_id': settings.CLIENT_ID,
+                                            'username': username})
 
-# GET /repos/:owner/:repo/contents/
 # PUT /repos/:owner/:repo/contents/:path
 
-def create_repo(filepath, auth_token, repo):
+def replicate_file(file_to_copy, username):
+    file_to_copy = base64.b64encode(bytes(file_to_copy, 'utf-8'))
+    contents_file = {'path': filepath,
+                     'message':'replicated file',
+                     'content': file_to_copy.decode("utf-8"),}
+    requests.put('https://api.github.com/repos/%s/selfreplicatingapp/contents/%s' % (username, json.dumps(contents_file)))
+    #response = requests.put('https://httpbin.org/put', data={'username': username, })
+
+def create_repo(auth_token, username):
+    
+    # response = requests.post('https://api.github.com/user/repos', auth=(access_token), data=json.dumps(payload))
+    response = requests.post('https://api.github.com/%s/repos' % username, data={'name': 'selfreplicatingapp',
+                                                                                 'description': 'This is an app that creates a copy of itself as a repo on github.',
+                                                                                 'homepage': 'selfreplicator.herokuapp.com',
+                                                                                 'auto_init': False})
+    
     # List of files to replicate
     appfiles = [
         'Procfile',                                  #gunicorn procfile
@@ -52,6 +67,7 @@ def create_repo(filepath, auth_token, repo):
         'selfreplicator/templates/results.html',
         'selfreplicator/templates/base.html',
         'selfreplicator/templates/index.html',
+        'selfreplicator/templates/error.html',
         'githubapps',                               # Main site folder for project
         'githubapps/static',
         'githubapps/static/humans.txt',
@@ -64,13 +80,4 @@ def create_repo(filepath, auth_token, repo):
         #filepath relative from working directory (should be where manage.py is)
         with open(filepath) as f:
             file_to_copy = f.read()
-            
-        file_to_copy = base64.b64encode(bytes(file_to_copy, 'utf-8'))
-        contents_file = {'path': filepath, 'message':'including file', 'content': file_to_copy.decode("utf-8")}
-        requests.put('https://api.github.com/repos/%s/%s/contents/%s' % (username, repo, json.dumps(contents_file)))
-    payload = {'name': repo, 'description': 'this is a self-replication app.', 'auto_init': False}
-    login = requests.post('https://api.github.com/user/repos', auth=(access_token), data=json.dumps(payload))
-
-    if login.status_code == 201:
-        return jsonify({'success' : 'Repo is replicated', 'user' : username})
-    return jsonify({'error' : login.json()['message']})
+            replicate_file(file_to_copy, username)
